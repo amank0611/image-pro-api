59e91817e4a647dff23bd0aa5ef2ef5abd09bc3b
Aman Nur <131132569+amank0611@users.noreply.github.com>
2023-04-18 11:44:12 +0530

    Add files via upload

---
+++ ImageProAPI.Infrastructure/DependencyResolver.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProAPI.Infrastructure
+{
+   public static class DependencyResolver
+    {
+        public static void AddDependencyResolver(this IServiceCollection services)
+        {
+             
+            //services.AddSingleton<ILoggerService, LogService>();
+            //services.AddScoped(typeof(IAutoMapConverter<,>), typeof(AutoMapConverter<,>));
+        }
+    }
+}
---
+++ ImageProAPI.Infrastructure/Persistence/DataContext/ImageProDBContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProAPI.Infrastructure.Persistence.DBContext
+{
+    public class ImageProDBContext
+    {
+    }
+}
